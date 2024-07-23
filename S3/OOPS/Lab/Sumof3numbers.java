8e49bb7ee625271746c46bb70a170c48f0e45bfc
GokulP <Pgokul695@gmail.com>
2024-07-23 17:10:07 +0530

    Added files

---
+++ S3/OOPS/Lab/Sumof3numbers.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class sum
+{
+    public static void main(String[] args) 
+    {
+        Scanner s =new Scanner(System.in);
+        int a,b,c;
+        System.out.println("Enter Three integers");
+        a=s.nextInt();
+        b=s.nextInt();
+        c=s.nextInt();
+        int sum=a+b+c;
+        System.out.println("The sum is"+sum);
+
+    }
+
+    
+}
