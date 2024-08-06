35efeb1a4afa8397054a39847ee40fef468614a8
GokulP <131006084+pgokul695@users.noreply.github.com>
2024-08-06 13:01:51 +0530

    Update matrixmul.java

--- S3/OOPS/Lab/matrixmul.java
+++ S3/OOPS/Lab/matrixmul.java
@@ -1,63 +1,59 @@
-import java.util.*;
-class matrixpro
-{
-	public static void main(String args[])
-	{
-		Scanner s = new Scanner(System.in);
-		int a[][] =new int[50][50];
-		int b[][] =new int[50][50];
-		int sum[][] =new int[50][50];
-		int r,c,i,j,c2,r2;
-		System.out.println("Enter the number of Columns of the First matrix");
-		c= s.nextInt();
-		System.out.println("Enter the number of Rows of the First matrix");
-		r= s.nextInt();
-		System.out.println("Enter the number of Columns of the Second matrix");
-		c2= s.nextInt();
-		System.out.println("Enter the number of Rows of the Second matrix");
-		r2= s.nextInt();
-		if (r!=c2)
-		{	System.out.println("The Matrices cant be Multiplied");
-			System.exit(0);
-		}
-		System.out.println("Enter the First matrix");
-		for(i=0;i<r;i++)
-		{
-			for(j=0;j<c;j++)
-			{
-				a[i][j]=s.nextInt();
-			}
-		}
-		System.out.println("Enter the Second matrix");
-		for(i=0;i<r2;i++)
-		{
-			for(j=0;j<c2;j++)
-			{
-				b[i][j]=s.nextInt();
-			}
-		}
-		for(i=0;i<r;i++)
-		{
-			for(j=0;j<c2;j++)
-			{
-				sum[i][j]=0;
-				for(int k=0;k<c2;k++)
-				{
-					sum[i][j]+=a[i][k]*b[k][j];
-				}
-			}
-		}
-		System.out.println("The product is");
-		System.out.println("");
-		for(i=0;i<r;i++)
-		{
-			for(j=0;j<c2;j++)
-			{
-				System.out.print("\t"+sum[i][j]);
-			}
-			System.out.println("");
-		}
-		s.close();
-	}
+import java.util.Scanner;
+
+class matrixmul {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("Enter the number of Rows of the First matrix");
+        int r1 = s.nextInt();
+        System.out.println("Enter the number of Columns of the First matrix");
+        int c1 = s.nextInt();
+        
+        System.out.println("Enter the number of Rows of the Second matrix");
+        int r2 = s.nextInt();
+        System.out.println("Enter the number of Columns of the Second matrix");
+        int c2 = s.nextInt();
+        
+        if (c1 != r2) {
+            System.out.println("The Matrices can't be Multiplied");
+            System.exit(0);
+        }
+        
+        int[][] a = new int[r1][c1];
+        int[][] b = new int[r2][c2];
+        int[][] product = new int[r1][c2];
+        
+        System.out.println("Enter the First matrix:");
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c1; j++) {
+                a[i][j] = s.nextInt();
+            }
+        }
+        
+        System.out.println("Enter the Second matrix:");
+        for (int i = 0; i < r2; i++) {
+            for (int j = 0; j < c2; j++) {
+                b[i][j] = s.nextInt();
+            }
+        }
+        
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c2; j++) {
+                product[i][j] = 0;
+                for (int k = 0; k < c1; k++) {
+                    product[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+        
+        System.out.println("The product is:");
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c2; j++) {
+                System.out.print(product[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        
+        s.close();
+    }
 }
-		
