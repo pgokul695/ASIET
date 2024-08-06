d19b66a79bfd75343b6010b81c6a23c8b48e4dc2
GokulP <131006084+pgokul695@users.noreply.github.com>
2024-08-06 12:13:56 +0530

    Update matrixmul.java

--- S3/OOPS/Lab/matrixmul.java
+++ S3/OOPS/Lab/matrixmul.java
@@ -7,11 +7,19 @@ class matrixpro
 		int a[][] =new int[50][50];
 		int b[][] =new int[50][50];
 		int sum[][] =new int[50][50];
-		int r,c,i,j;
-		System.out.println("Enter the number of Columns of the matrix");
+		int r,c,i,j,c2,r2;
+		System.out.println("Enter the number of Columns of the First matrix");
 		c= s.nextInt();
-		System.out.println("Enter the number of Rows of the matrix");
+		System.out.println("Enter the number of Rows of the First matrix");
 		r= s.nextInt();
+		System.out.println("Enter the number of Columns of the Second matrix");
+		c2= s.nextInt();
+		System.out.println("Enter the number of Rows of the Second matrix");
+		r2= s.nextInt();
+		if (r!=c2)
+		{	System.out.println("The Matrices cant be Multiplied");
+			System.exit(0);
+		}
 		System.out.println("Enter the First matrix");
 		for(i=0;i<r;i++)
 		{
@@ -21,19 +29,19 @@ class matrixpro
 			}
 		}
 		System.out.println("Enter the Second matrix");
-		for(i=0;i<r;i++)
+		for(i=0;i<r2;i++)
 		{
-			for(j=0;j<c;j++)
+			for(j=0;j<c2;j++)
 			{
 				b[i][j]=s.nextInt();
 			}
 		}
 		for(i=0;i<r;i++)
 		{
-			for(j=0;j<c;j++)
+			for(j=0;j<c2;j++)
 			{
 				sum[i][j]=0;
-				for(int k=0;k<c;k++)
+				for(int k=0;k<c2;k++)
 				{
 					sum[i][j]+=a[i][k]*b[k][j];
 				}
@@ -43,7 +51,7 @@ class matrixpro
 		System.out.println("");
 		for(i=0;i<r;i++)
 		{
-			for(j=0;j<c;j++)
+			for(j=0;j<c2;j++)
 			{
 				System.out.print("\t"+sum[i][j]);
 			}
@@ -53,6 +61,3 @@ class matrixpro
 	}
 }
 		
-		
-
-
