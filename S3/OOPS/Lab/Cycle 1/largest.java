9aa8ef1e5a678d7a9ea5f853e1616db69d6ba733
GokulP <131006084+pgokul695@users.noreply.github.com>
2024-08-06 19:54:21 +0530

    Create largest.java

---
+++ S3/OOPS/Lab/Cycle 1/largest.java
@@ -0,0 +1,23 @@
+//Write a Java Program to find the largest of N numbers, without using array.
+//Gokulp
+//30
+import java.util.*;
+
+class largest{
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the number of values");
+        int n = s.nextInt();
+        int largest =0;
+	      for (int i = 0; i < n; i++) 
+	          {
+              System.out.print("Enter number " + (i + 1) + ": ");
+              int num = s.nextInt();
+              if (num > largest) 
+                largest = num;
+            }
+        System.out.println("The largest number is: " + largest);
+        s.close();
+    }
+}
