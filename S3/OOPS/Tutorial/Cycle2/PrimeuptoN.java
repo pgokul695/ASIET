531893d5e949235a11214486abe028ddaee5d0d8
GokulP <131006084+pgokul695@users.noreply.github.com>
2024-08-11 10:52:16 +0530

    Create PrimeuptoN.java

---
+++ S3/OOPS/Tutorial/Cycle2/PrimeuptoN.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+class PrimeNumbers {
+
+    boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        if (num == 2) {
+            return true;
+        }
+        if (num % 2 == 0) {
+            return false;
+        }
+        for (int i = 3; i <= Math.sqrt(num); i += 2) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void printPrimesUpTo(int n) {
+        System.out.println("Prime numbers up to " + n + ":");
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the limit  ");
+        int n = s.nextInt();
+        printPrimesUpTo(n);
+        s.close();
+    }
+}
