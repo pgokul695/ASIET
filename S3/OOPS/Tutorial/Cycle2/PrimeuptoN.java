7c16d269daae9430024d76bbbe1ee6fcba834567
GokulP <131006084+pgokul695@users.noreply.github.com>
2024-08-13 12:24:36 +0530

    Update PrimeuptoN.java

--- S3/OOPS/Tutorial/Cycle2/PrimeuptoN.java
+++ S3/OOPS/Tutorial/Cycle2/PrimeuptoN.java
@@ -6,6 +6,14 @@ class PrimeNumbers {
         if (num <= 1) {
             return false;
         }
+        if (num == 2) {import java.util.Scanner;
+
+class PrimeNumbers {
+
+    boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
         if (num == 2) {
             return true;
         }
@@ -30,10 +38,48 @@ class PrimeNumbers {
         System.out.println();
     }
 
+    public static void main(String args[]) 
+    {
+        int j=1,n,i=0,l;
+		l=args.length();
+        for(i=l;i>-1;i++)
+        {
+        	n=(Integer.ParseInt(args[i])*j);
+        	j*=10;
+        }
+    }
+}
+
+            return true;
+        }
+        if (num % 2 == 0) {
+            return false;
+        }
+        for (int i = 3; i <= Math.sqrt(num); i += 2) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void printPrimesUpTo(int n) {
+        System.out.println("Prime numbers up to " + n + ":");
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter the limit  ");
-        int n = s.nextInt();
+        int j,n,l,i=0;
+        while(String[i]!="")
+            {
+                i++
+                    }
+        
         printPrimesUpTo(n);
         s.close();
     }
