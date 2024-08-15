73de8315db1ea590add138bc8fef904d7e99ffc0
GokulP <131006084+pgokul695@users.noreply.github.com>
2024-08-15 08:40:08 +0530

    Update inheritance.java

--- S3/OOPS/Lab/Cycle3/Calculatesal/inheritance.java
+++ S3/OOPS/Lab/Cycle3/Calculatesal/inheritance.java
@@ -5,6 +5,7 @@ class employee {
     long phone;
     String name, address;
     double salary;
+    int workExperience;
 
     void printSal() {
         System.out.println("Salary: " + salary);
@@ -13,16 +14,40 @@ class employee {
 
 class officer extends employee {
     String special;
+
+    void calculateSal() {
+        double baseSalary = 30000;
+        if (special.equalsIgnoreCase("Technical")) {
+            salary = baseSalary + 5000;
+        } else if (special.equalsIgnoreCase("Managerial")) {
+            salary = baseSalary + 8000;
+        } else {
+            salary = baseSalary;
+        }
+        salary += workExperience * 1000;
+    }
 }
 
 class manager extends employee {
     String dep;
+
+    void calculateSal() {
+        double baseSalary = 50000;
+        if (dep.equalsIgnoreCase("HR")) {
+            salary = baseSalary + 6000;
+        } else if (dep.equalsIgnoreCase("Finance")) {
+            salary = baseSalary + 8000;
+        } else {
+            salary = baseSalary;
+        }
+        salary += workExperience * 1500;
+    }
 }
 
 class print {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        
+
         officer a = new officer();
         manager b = new manager();
 
@@ -36,12 +61,13 @@ class print {
         a.address = s.nextLine();
         System.out.println("Enter the phone number:");
         a.phone = s.nextLong();
-        System.out.println("Enter the salary:");
-        a.salary = s.nextDouble();
         System.out.println("Enter the Specialization:");
         s.nextLine();
         a.special = s.nextLine();
-        
+        System.out.println("Enter the Work Experience (in years):");
+        a.workExperience = s.nextInt();
+        a.calculateSal();
+
         System.out.println("Enter the Manager's details");
         System.out.println("Enter The Name:");
         b.name = s.nextLine();
@@ -52,14 +78,16 @@ class print {
         b.address = s.nextLine();
         System.out.println("Enter the phone number:");
         b.phone = s.nextLong();
-        System.out.println("Enter the salary:");
-        b.salary = s.nextDouble();
         System.out.println("Enter the Department:");
         s.nextLine();
         b.dep = s.nextLine();
-        
+        System.out.println("Enter the Work Experience (in years):");
+        b.workExperience = s.nextInt();
+        b.calculateSal();
+
         System.out.println("The details of the officer are:");
         System.out.println("Name:\t" + a.name);
+	s.nextLine();
         System.out.println("Age:\t" + a.age);
         System.out.println("Address:\t" + a.address);
         System.out.println("Phone number:\t" + a.phone);
