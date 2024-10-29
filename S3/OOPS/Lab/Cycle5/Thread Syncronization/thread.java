940d1ff601c63257123780f747a1a2be14fe4816
GokulP <131006084+pgokul695@users.noreply.github.com>
2024-10-29 19:10:19 +0530

    Add files via upload

---
+++ S3/OOPS/Lab/Cycle5/Thread Syncronization/thread.java
@@ -0,0 +1,53 @@
+class BankAccount {
+    private int balance = 1000;
+
+    public synchronized void deposit(int amount) {
+        balance += amount;
+        System.out.println(Thread.currentThread().getName() + " deposited: " + amount + ", New Balance: " + balance);
+    }
+
+    public synchronized void withdraw(int amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            System.out.println(Thread.currentThread().getName() + " withdrew: " + amount + ", New Balance: " + balance);
+        } else {
+            System.out.println(Thread.currentThread().getName() + " tried to withdraw: " + amount + " but insufficient funds. Balance: " + balance);
+        }
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+}
+
+class AccountHolder extends Thread {
+    private BankAccount account;
+
+    public AccountHolder(BankAccount account) {
+        this.account = account;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 3; i++) {
+            account.deposit(100);
+            account.withdraw(200);
+        }
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        BankAccount sharedAccount = new BankAccount();
+
+        AccountHolder holder1 = new AccountHolder(sharedAccount);
+        AccountHolder holder2 = new AccountHolder(sharedAccount);
+
+        holder1.setName("AccountHolder 1");
+        holder2.setName("AccountHolder 2");
+
+        holder1.start();
+        holder2.start();
+    }
+}
+
