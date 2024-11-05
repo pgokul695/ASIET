44aa6cdf2d846d0294a9e6e431327423912a65a3
GokulP <131006084+pgokul695@users.noreply.github.com>
2024-11-05 10:52:55 +0530

    Add files via upload

---
+++ S3/OOPS/Tutorial/Cycle 4/Package/HelloWorld.java
@@ -0,0 +1,8 @@
+package mypackage;
+
+public class HelloWorld {
+    public void greet() {
+        System.out.println("Hello from mypackage!");
+    }
+}
+
---
+++ S3/OOPS/Tutorial/Cycle 4/Package/test.java
@@ -0,0 +1,8 @@
+import mypackage.HelloWorld;
+class main {
+    public static void main(String[] args) {
+        HelloWorld obj = new HelloWorld();
+        obj.greet();
+    }
+}
+
